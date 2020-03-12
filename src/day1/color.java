ddc4f318780db3a934667c055fa7e069686c8cdc
dilixat928@gmail.com <dilixat928@gmail.com>
2020-03-11 17:14:11 -0700

    modified array colors

--- src/day1/color.java
+++ src/day1/color.java
@@ -3,7 +3,7 @@ package day1;
 public class color {
 
     public static void main(String[] args) {
-        String[]colors={"red","black","green"};
+        String[]colors={"red","black","green","yellow"};
     }
 
 
